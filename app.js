5686d4dfc3e08ebd262e5550f8628e4472eca8ab
Pavaman Dabeer <pavamandabeer@gmail.com>
2022-09-14 09:49:02 +0530

    import routes, controllers and write dummy routes

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require('express')
 
 const app = express()
 
+// import all routes here
+const home = require('./routes/home');
 
+
+//router middleware
+app.use('/api/v1/', home)
 
 
 
