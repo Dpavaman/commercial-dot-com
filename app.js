13729ad81526823d356d5f4ee6a52e95b8b798a5
Pavaman Dabeer <pavamandabeer@gmail.com>
2022-09-14 10:13:45 +0530

    injected documentation

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
+const { urlencoded } = require('express');
 const express = require('express')
-
+// use dotenv here if required
 const app = express()
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+
+
+//for swagger documentation
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml')
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+//regular middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+
+//cookies and file middleware
+app.use(cookieParser());
+app.use(fileUpload())
+
+// morgan middleware
+app.use(morgan('tiny'))
 
 // import all routes here
 const home = require('./routes/home');
